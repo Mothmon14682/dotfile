bd5a32c84813c0a0654d67284ea1fafbb02cca43
Mothmon14682 <huynhanhtuan14686@gmail.com>
2026-05-11 15:42:35 +0700

    Revamp something to actually create a theme of my own

--- scripts/.config/scripts/background.sh
+++ scripts/.config/scripts/background.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
-if [[ "$1" != "--child" ]]; then
-  nohup "$0" --child >/dev/null 2>&1 &
-  exit
-fi
+# if [[ "$1" != "--child" ]]; then
+#   nohup "$0" --child >/dev/null 2>&1 &
+#   exit
+# fi
+# 
+# val_file="$HOME/.config/waybar/scripts/background/val.txt"
+# path="$HOME/background/static"
+# 
+# read -r old_pic < "$val_file"
+# 
+# pic="$(find "$path" -type f | shuf -n 1)"
+# while [ "$old_pic" = "$pic" ]; do
+#     pic="$(find "$path" -type f | shuf -n 1)"
+# done
+# 
+# echo "$pic" > "$val_file"
+# 
+# killall swaybg 2>/dev/null
+# killall feh 2>/dev/null
+# killall mpvpaper 2>/dev/null
+# 
+# is_wayland=$(echo $WAYLAND_DISPLAY)
+# if [[ -z "$is_wayland"  ]]; then
+#     feh --bg-fill "$pic"
+# else
+#     swaybg -i "$pic" -m fill & disown
+# fi
+# 
+# wal -i "$pic" -n -q -e
 
-val_file="$HOME/.config/waybar/scripts/background/val.txt"
-path="$HOME/background/static"
+pic=$(cat ~/.cache/wal/wal)
 
-read -r old_pic < "$val_file"
-
-pic="$(find "$path" -type f | shuf -n 1)"
-while [ "$old_pic" = "$pic" ]; do
-    pic="$(find "$path" -type f | shuf -n 1)"
-done
-
-echo "$pic" > "$val_file"
-
-killall swaybg 2>/dev/null
-killall feh 2>/dev/null
-killall mpvpaper 2>/dev/null
-
-is_wayland=$(echo $WAYLAND_DISPLAY)
-if [[ -z "$is_wayland"  ]]; then
-    feh --bg-fill "$pic"
-else
-    swaybg -i "$pic" -m fill & disown
-fi
-
+feh --bg-fill "$pic"
 wal -i "$pic" -n -q -e
--- scripts/.config/scripts/polybar_launch.sh
+++ scripts/.config/scripts/polybar_launch.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
+polybar-msg cmd quit
 
-# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-until xrandr --query | grep -q " connected"; do
-  sleep 0.2
-done
-
-# Give tray + dex time to settle
-sleep 1
-
-polybar bar1 &
-
+echo "Bars launched..."
