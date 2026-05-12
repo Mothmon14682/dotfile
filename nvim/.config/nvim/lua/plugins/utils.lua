bded394d99d0cf046ecb99372de6e880ee3cffa9
Mothmon14682 <huynhanhtuan14686@gmail.com>
2026-05-12 09:33:26 +0700

    change how nvim render color

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -19,7 +19,13 @@ return {
     { "nvim-tree/nvim-web-devicons", lazy = true },
     { "brenoprata10/nvim-highlight-colors",
       config = function ()
-          require('nvim-highlight-colors').setup({})
+          require('nvim-highlight-colors').setup({
+              render = 'virtual',
+              virtual_symbol_prefix = ' ',
+              virtual_symbol_suffix = '',
+              virtual_symbol = '■',
+              virtual_symbol_position = 'eow',
+          })
       end
     },
 }
