e754ee2c3a435c850b4a50e9f9d8a0a95031d11b
Mothmon14682 <huynhanhtuan14686@gmail.com>
2026-06-20 17:44:46 +0700

    Fixed net_menu.sh

--- scripts/.config/scripts/net_menu.sh
+++ scripts/.config/scripts/net_menu.sh
@@ -1,54 +1,80 @@
 #!/usr/bin/env bash
 
-connected=$(nmcli -t -f NAME connection show)
-selection=$(nmcli -t -f SECURITY,SSID device wifi | sort -u | sed -E 's/(WPA)\S//g; s/ //g; /:[[:space:]]*$/d; s/:/ /' | rofi -dmenu -p "SSID: ")
-ssid=${selection# }
+rofi_selection() {
+    rofi -dmenu -theme-str "textbox-prompt-colon { 
+                                str: '$1'; 
+                                padding: 10px 14px 10px 14px; 
+                            }
+                            
+                            entry {
+                                placeholder: 'Amiya, find me this wifi...';
+                            }
+                           "
+}
 
-success="Connected to: $ssid"
+rofi_password() {
+    rofi -dmenu -password -theme-str "textbox-prompt-colon { 
+                                str: 'Password:'; 
+                                padding: 10px 14px 10px 14px; 
+                            }
+                            
+                            entry {
+                                placeholder: 'Don't tell Kal\'sit this...';
+                            }
+                            
+                            mainbox {
+                                children: [ 'inputbar' ];
+                            }
+                           "
+}
 
-if [[ -z "$selection" ]]; then
-    exit 0
-elif [[ "$selection" =~ "" ]]; then
-    pass=$(rofi -dmenu -password -p "Password: ")
+check_and_notify() {
+    local status=$1
 
-    if [[ -z "$pass" ]]; then
-        notify-send -u critical "Net Menu" "Need to pass in a password to connect"
-        exit 1
-    fi
-
-    if echo "$connected" | grep -Fxq "$ssid"; then
-        if nmcli connection up "$ssid" > /dev/null; then
-            notify-send -u normal "Net Menu" "$success"
-            exit 0
-        else
-            notify-send -u critical "Net Menu" "Connection failed"
-        fi
-
+    if [[ $status -eq 0 ]]; then
+        notify-send "Net menu" "$2" -u low -t 5000 -r 80211
+        exit 0
     else
-        if nmcli device wifi connect "$ssid" password "$pass" > /dev/null; then
-            notify-send -u normal "Net Menu" "$success"
-            exit 0
-        else
-            notify-send -u critical "Net Menu" "Failed to connect to $ssid"
-            nmcli connection delete "$ssid"
-        fi
+        notify-send "Net menu" "$3" -u critical -t 0 -r 80211
     fi
-else
-    if echo "$connected" | grep -Fxq "$ssid"; then
-        if nmcli connection up "$ssid" > /dev/null; then
-            notify-send -u normal "Net Menu" "$success"
-            exit 0
-        else
-            notify-send -u critical "Net Menu" "Connection failed"
+}
+
+while true; do
+    nmcli device wifi rescan
+
+    connected=$(nmcli -t -f NAME connection show)
+    selection=$(nmcli -t -f SECURITY,SSID device wifi list | sort -u | awk -F: '$2 != ""' | sed -E 's/^[^:]+:/ /' | sed -E 's/^:/ /' | rofi_selection 'SSID:')
+
+    if [[ -z "$selection" ]]; then
+        exit 0
+    fi
+
+    if [[ "$selection" == *""* ]]; then
+        ssid=${selection# }
+        security=1
+    elif [[ "$selection" == *""* ]]; then
+        ssid=${selection# }
+        security=0
+    fi
+
+
+    if [[ "$connected" =~ "$ssid" ]]; then
+        nmcli connection up "$ssid"
+        status=$?
+        check_and_notify $status "Successfully bring $ssid up" "Failed to bring $ssid up"
+    elif [[ $security -eq 1 ]]; then
+        password=$(rofi_password)
+
+        if [[ -z $password ]]; then
+            continue
         fi
 
-    else
-        if nmcli device wifi connect "$ssid" > /dev/null; then
-            notify-send -u normal "Net Menu" "$success"
-            exit 0
-        else
-            notify-send -u critical "Net Menu" "Failed to connect to $ssid"
-            nmcli connection delete "$ssid"
-        fi
+        nmcli device wifi connect "$ssid" password "$password"
+        status=$?
+        check_and_notify $status "Successfully connected to $ssid" "Failed to connect to $ssid"
+    elif [[ $security -eq 0 ]]; then
+        nmcli device wifi connect "$ssid"
+        status=$?
+        check_and_notify $status "Successfully connected to $ssid" "Failed to connect to $ssid"
     fi
-fi
+done
